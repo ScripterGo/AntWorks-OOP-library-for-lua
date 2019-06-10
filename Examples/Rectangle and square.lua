4e12733e97e67cc8946f86ca9bff8e95ae0517d3
ScripterGo <48377932+ScripterGo@users.noreply.github.com>
2019-06-10 16:48:49 +0200

    Update Rectangle and square.lua

--- Examples/Rectangle and square.lua
+++ Examples/Rectangle and square.lua
@@ -1,1 +1,25 @@
 
+local RectangleClass = AntClass("Rectangle")
+RectangleClass:SetConstruct(function(self, Width, Height)
+	self.Width = Width
+	self.Height = Height
+	
+	self:LockProperty("Width", "Height")
+	return self
+end)
+
+function RectangleClass:GetArea()
+	return self.Width * self.Height
+end
+
+local SquareClass = AntClass("Square")
+SquareClass:Inherit(RectangleClass)
+
+SquareClass:SetConstruct(function(self, Length)
+	self = RectangleClass.new(Length, Length)
+	return self
+end)
+
+local Square = SquareClass.new(4)
+print(Square:GetArea())
+print(Square:Is("Rectangle"))
