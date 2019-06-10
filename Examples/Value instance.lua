aa12b087d7cede084abf5c0d20d5420bd495e2eb
ScripterGo <48377932+ScripterGo@users.noreply.github.com>
2019-06-10 16:34:47 +0200

    Update Value instance.lua

--- Examples/Value instance.lua
+++ Examples/Value instance.lua
@@ -1,9 +1,28 @@
 
 
 
+local AntClass = require("AntClass")
+local ValueInstanceClass = AntClass.new("ValueInstance")
 
+ValueInstanceClass:SetConstruct(function(self, ValueType)
+	assert(script[ValueType], "Incompatible datatype")
+	
+	local SpecificClass = require(script[ValueType])
+	self = SpecificClass.new()
+	
+	self.Value = nil 
+	
+	self:SuperLock() ---Locking everything
+	self:Unlock("Value") ---Unlocking "Value" making "Value" then only non locked index
+	self:LimitDataType("Value", ValueType) ----Limiting the datatype of the value at index to ValueType
+	self:Inherit(ValueInstanceClass) --Yep the instantiated object is inheriting
+	
+	return self
+end)
 
+function ValueInstanceClass:Idk()
+	print(self.Value)
+end
 
-
-
-
+local numberValue = ValueInstanceClass.new("number")
+numberValue:Idk()
